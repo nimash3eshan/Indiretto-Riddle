5d869c461626809d307a3608ec41937501ac69d6
nimash eshan <81243419+nimash3eshan@users.noreply.github.com>
2022-04-10 03:45:23 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,42 @@
+let MCQS = [
+{
+    video: "https://www.dailymotion.com/embed/video/k4tI0EcJpeuNqhxRdKK",
+    question: "Who killed Andrew?",
+    choice1: "Anne",
+    choice2: "Mike",
+    choice3: "Otto",
+    answer: 2
+},
+
+{
+    video: "https://www.dailymotion.com/embed/video/k7p8zOoyx7JnwaxRdKL",
+    question: "Who's the girl from party?",
+    choice1: "laila",
+    choice2: "christie",
+    choice3: "Natalie",
+    answer: 2
+},
+{
+    video: "https://www.dailymotion.com/embed/video/k6P5Qn4vUMPH5cxRdKM?autoplay=1",
+    question: "Who is Lying?",
+    choice1: "Sarah",
+    choice2: "Lily",
+    choice3: "Annie",
+    answer: 1
+},
+{
+    video: "https://www.dailymotion.com/embed/video/k4BWVJLjaNHIrJxRdKN?autoplay=1",
+    question: "Who is Lying",
+    choice1: "Harry",
+    choice2: "Anna",
+    choice3: "Helen",
+    answer: 1
+},
+{
+    video: "https://www.dailymotion.com/embed/video/k7ldzMvKmmN7NUxRdKO?autoplay=1",
+    question: "Who killed the Boy?",
+    choice1: "Maid",
+    choice2: "BabySitter",
+    choice3: "Cook",
+    answer: 0
+}];
